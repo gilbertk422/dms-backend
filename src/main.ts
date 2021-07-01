255a66b39c7f1cc3a8c7b5531b641cb253581ff4
Gilbert Kim <gilbertk422@gmail.com>
2021-07-01 18:41:55 -0400

    feat: update cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001'],
+    origin: ['http://localhost:3001', 'https://dms-dev.alethea.ai', 'https://dms.alethea.ai'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTION',
     preflightContinue: false,
     optionsSuccessStatus: 204,
