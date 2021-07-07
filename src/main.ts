1aca7ed9fb79c4586fc84fbe15333e3ceb3d1bfc
Gilbert Kim <gilbertk422@gmail.com>
2021-07-07 04:44:23 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'https://dms-dev.alethea.ai', 'https://dms.alethea.ai'],
+    origin: ['http://localhost:3001', 'http://localhost:3000', 'https://dms-dev.alethea.ai', 'https://dms.alethea.ai'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTION',
     preflightContinue: false,
     optionsSuccessStatus: 204,
