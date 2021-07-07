608bd220654c8712048dd88cf7879924ee69d75c
Gilbert Kim <gilbertk422@gmail.com>
2021-07-07 04:39:04 +0100

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('DMS API docs')
+    .setDescription('The DMS API description')
+    .setVersion('1.0')
+    .addTag('dms')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors({
     origin: ['http://localhost:3001', 'https://dms-dev.alethea.ai', 'https://dms.alethea.ai'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTION',
