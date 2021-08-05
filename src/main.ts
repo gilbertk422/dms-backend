1f877983d987f8166cdaae5f6411441a5a43cb30
Ubuntu <ubuntu@ip-172-31-10-204.ec2.internal>
2021-08-05 21:10:15 +0000

    update backend

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import config from './config';
-import { urlencoded, json } from 'express';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -33,8 +33,8 @@ async function bootstrap() {
     optionsSuccessStatus: 204,
   });
 
-  app.use(json({ limit: '50mb' }));
-  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
   await app.listen(config.port);
 }
